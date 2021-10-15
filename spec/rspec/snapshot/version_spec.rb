fddecf903f35046bed5274f58dd87f66f134680c
Mike Levin <levinmr@upmc.edu>
2021-10-15 14:31:10 -0400

    Bump version to 2.0.1

--- spec/rspec/snapshot/version_spec.rb
+++ spec/rspec/snapshot/version_spec.rb
@@ -3,7 +3,7 @@
 require 'spec_helper'
 
 describe RSpec::Snapshot::VERSION do
-  it 'is set to 2.0.0' do
-    expect(subject).to eq('2.0.0')
+  it 'is set to 2.0.1' do
+    expect(subject).to eq('2.0.1')
   end
 end
