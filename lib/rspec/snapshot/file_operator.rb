7e9ceabe0950dd44df8465b2ce48184b679b302f
phyzical <5182053+phyzical@users.noreply.github.com>
2025-04-14 15:07:45 +0800

    add logic to raise when the snapshot is missing

--- lib/rspec/snapshot/file_operator.rb
+++ lib/rspec/snapshot/file_operator.rb
@@ -42,9 +42,8 @@ module RSpec
       # Writes the value to file, overwriting the file contents if either of the
       # following is true:
       # * The snapshot file does not already exist.
+      # * If it does not exist and the RAISE_ON_SNAPSHOT_CREATE environment variable is set throw
       # * The UPDATE_SNAPSHOTS environment variable is set.
-      #
-      # TODO: Do not write to file if running in CI mode.
       #
       # @param [String] snapshot_name The snapshot name.
       # @param [String] value The value to write to file.
@@ -68,7 +67,11 @@ module RSpec
       end
 
       private def file_does_not_exist?
-        !File.exist?(@snapshot_path)
+        return false if File.exist?(@snapshot_path)
+        if ENV.fetch('RAISE_ON_SNAPSHOT_CREATE', 'false') == 'true'
+          raise "Snapshot file does not exist #{@snapshot_path}, set to raise when missing"
+        end
+        true
       end
     end
   end
